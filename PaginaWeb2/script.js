4719f10dc2c9c87f3b20967cfff5e0b161ca173a
Jean David Vasquez <116390051+JeanDavidVasquez@users.noreply.github.com>
2024-07-26 13:21:42 -0500

    Delete PaginaWeb2 directory

--- PaginaWeb2/script.js
+++
@@ -1,8 +0,0 @@
-// Agrega la funcionalidad de zoom al hacer clic en las imágenes
-const images = document.querySelectorAll('.zoom');
-
-images.forEach(image => {
-    image.addEventListener('click', () => {
-        image.classList.toggle('zoomed');
-    });
-});
